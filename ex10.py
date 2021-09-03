66e8a76dfc8d90026a41abdb053059add24711e5
raioliva21 <89752816+raioliva21@users.noreply.github.com>
2021-09-03 14:27:10 -0400

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,3 +1,12 @@
+"""
+Ejericio 10
+Una empresa constructora vende terrenos con la forma A de la figura. Se requiere crear un programa
+para obtener el área respectiva de un terreno de medidas de cualquier valor.
+Para resolver este problema se debe identificar que la forma A está compuesta por dos figuras: un
+triángulo de base B y de altura (A - C); y por otro lado, un rectángulo que tiene base B y altura C y
+la suma de ambas áreas es el resultado final.
+"""
+
 unidad_medida = input("Ingrese unidad de medida\n")
 # variable "base_figuras" refiere tanto a la base de triangulo como rectangulo (de igual medida)
 base_figuras = int(input("Ingrese medida de la base del rectangulo (o triangulo)\n"))
